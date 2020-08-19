3e3bc60fba8ffd00e9e80d63ded9cdf2cd70bf31
Mrt1453701- <degirmenci_murat@yahoo.com>
2020-08-18 20:04:17 -0400

    AllClassNotes

--- src/Day43_Static/Variables.java
+++ src/Day43_Static/Variables.java
@@ -1,38 +1,40 @@
 package Day43_Static;
-
 public class Variables {
 
-    int d;   // instance variable belongs to the object
-
-    static  int s;   // belongs to the class
+    int d;
+    static int s;
 
     public void method2(){
-        System.out.println(d); // instance method always except instance variable
+        System.out.println(d);
         System.out.println(s);
     }
 
+
     public static void main(String[] args) {
-        //System.out.println(a);
+        // System.out.println(a);
         System.out.println(s);
 
-       // System.out.println(d); static does not accepts instance  only way create an object
+        //   System.out.println(d);  // static ONLY accepts static
 
-        Variables obj =new Variables();
+        Variables obj = new Variables();
         System.out.println(obj.d);
+
+
 
     }
 
     public static void method1(){
-
-        int a;      // local variable we should initialize
+        int a;
 
         if(true){
-            int b=20;
+            int b = 20;
         }
+
+        // System.out.println(b);
+
+        //   System.out.println(a);
 
     }
 
 
-
 }
-
