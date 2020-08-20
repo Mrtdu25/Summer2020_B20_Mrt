33e7fd0c958909b31b8a4f832746544b8262b2fe
Mrt1453701- <degirmenci_murat@yahoo.com>
2020-08-19 22:06:03 -0400

    AllClassNotes.java

---
+++ src/Day44_Constructor/Dog.java
@@ -0,0 +1,9 @@
+package Day44_Constructor;
+
+public class Dog {
+    String name;
+    String breed;
+    String size;
+    int age;
+    String color;
+}
---
+++ src/Day44_Constructor/StudentObjects.java
@@ -0,0 +1,22 @@
+package Day44_Constructor;
+
+public class StudentObjects {
+
+    public static void main(String[] args) {
+
+        Student student1 = new Student("Mike", 18, 'M');
+        System.out.println(student1);
+
+        Student student2 = new Student("Rakhat", 28, 'F');
+        System.out.println(student2);
+
+        Student student3 = new Student("Belek", 30, 'M');
+        System.out.println(student3);
+
+
+
+
+    }
+
+
+}
