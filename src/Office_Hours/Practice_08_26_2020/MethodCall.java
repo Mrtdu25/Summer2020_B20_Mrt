ca25e27ace02d159825f94cdab9415710bdd53e4
Mrt1453701- <degirmenci_murat@yahoo.com>
2020-08-26 18:32:14 -0400

    AllClassNotes

---
+++ src/Office_Hours/Practice_08_26_2020/MethodCall.java
@@ -0,0 +1,13 @@
+package Office_Hours.Practice_08_26_2020;
+public class MethodCall {
+
+    public static void main(String[] args) {
+
+        double[] arr1={1.5,2.5,0.5,6.5,7.5};
+        Practice2.printArray(arr1);
+
+
+
+
+    }
+}
