c1bd8b2ab7c08d5c99cf8acb160e02d33eed2947
Mrt1453701- <degirmenci_murat@yahoo.com>
2020-08-26 17:16:45 -0400

    AllClassNotes

---
+++ src/Office_Hours/Practice_08_26_2020/Practice1.java
@@ -0,0 +1,19 @@
+package Office_Hours.Practice_08_26_2020;
+
+public class Practice1 {
+    public static void main(String[] args) {
+
+        int[] arr1 = {1, 2, 3, 4, 5, 6};
+        for (int i=0; i<=arr1.length-1; i++){
+            System.out.println(arr1[i]);
+        }
+
+        int[] arr2 = {1, 2, 3, 4, 5, 6};
+
+        for (int each:arr2){
+            System.out.print(each+" ");
+        }
+        System.out.println();
+
+    }
+}
